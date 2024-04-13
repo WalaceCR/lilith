898659c0353f066a27a7b4a957ed70212cdd0e9b
WalaceCR <Cavaleiro88@@>
2024-04-13 14:18:50 -0300

    Cinfra: onversão para gradle

---
+++ src/main/java/br/com/lilith/LilithApplication.java
@@ -0,0 +1,14 @@
+package br.com.lilith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilithApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LilithApplication.class, args);
+	}
+
+}
