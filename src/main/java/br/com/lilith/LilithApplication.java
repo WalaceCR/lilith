9d2a05cbf36a448cfb8c21a0c49d241d5c63bc80
WalaceCR <Cavaleiro88@@>
2024-04-14 13:02:43 -0300

    Funcionando

--- src/main/java/br/com/lilith/LilithApplication.java
+++ src/main/java/br/com/lilith/LilithApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LilithApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(LilithApplication.class, args);
 	}
 
